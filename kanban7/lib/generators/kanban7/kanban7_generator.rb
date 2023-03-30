57e3f9c95b360c8db575b033b1995b21207c2356
theforestvn88 <theforestvn88@gmail.com>
2023-03-30 20:33:53 +0700

    validate & force to add position migrations

--- kanban7/lib/generators/kanban7/kanban7_generator.rb
+++ kanban7/lib/generators/kanban7/kanban7_generator.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 require 'rails/generators/base'
+require "rails/generators/active_record"
 
 module Devise
   module Generators
         class Kanban7Generator < Rails::Generators::Base
-            # include Rails::Generators::ResourceHelpers
+            include ActiveRecord::Generators::Migration
+
             namespace "kanban7"
             source_root File.expand_path("../../templates/", __FILE__)
 
@@ -13,8 +15,12 @@ module Devise
             argument :list, required: true
             argument :card, required: true
 
-            def create_controllers
+            def validate
                 @board_model = board.to_s.classify.constantize
+                unless @board_model.superclass == ApplicationRecord
+                    raise ArgumentError, "board model(#{@board_model}) should be an ApplicationRecord"
+                end
+
                 @board_name = @board_model.name.downcase
                 @boards_name = @board_name.pluralize
 
@@ -22,13 +28,37 @@ module Devise
                     @fixed_lists = true
                 else
                     @list_model = list.to_s.classify.constantize
+                    unless @list_model.superclass == ApplicationRecord
+                        raise ArgumentError, "list model(#{@list_model}) should be an ApplicationRecord"
+                    end
+
                     @list_name = @list_model.name.downcase
                     @lists_name = @list_name.pluralize
                 end
 
                 @card_model = card.to_s.classify.constantize
+                unless @card_model.superclass == ApplicationRecord
+                    raise ArgumentError, "card model(#{@card_model}) should be an ApplicationRecord"
+                end
+
                 @card_name = @card_model.name.downcase
                 @cards_name = @card_name.pluralize
+
+            end
+
+            def scaffold
+                @migration_version = "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
+                unless @list_model.columns_hash.key?("position")
+                    @table_name = list.tableize
+                    migration_template "migrations/add_position.rb", "#{db_migrate_path}/add_position_to_#{@table_name}.rb"
+                end
+
+                unless @card_model.columns_hash.key?("position")
+                    @table_name = card.tableize
+                    migration_template "migrations/add_position.rb", "#{db_migrate_path}/add_position_to_#{@table_name}.rb"
+                end
+
+                puts "!!! Please run `rails db:migrate` to add :position columns to kanban-model table" if @table_name
 
                 template "controllers/boards_controller.rb", "app/controllers/#{@board_name}_kanban_controller.rb"
                 template "views/boards/_header.html.erb", "app/views/#{@board_name}_kanban/boards/_header.html.erb"
